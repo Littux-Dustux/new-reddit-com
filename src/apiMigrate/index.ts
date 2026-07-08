1c718b9f98839069db980735987ade74879984c7
littux <littux.dustux@gmail.com>
2026-07-08 22:16:26 +0530

    Make telemetry work

--- src/apiMigrate/index.ts
+++ src/apiMigrate/index.ts
@@ -1,5 +1,5 @@
 import { getLogger } from "../logging/logger";
-import { addInterceptor } from "../interceptor/xhr";
+import { addInterceptor, type InterceptorHandler } from "../interceptor/xhr";
 
 import { gqlMigrationInterceptor } from "./gql";
 import { gatewayMigratorInterceptor } from "./gateway";
@@ -14,9 +14,25 @@ export function initAPIMigratorInterceptors() {
 	addInterceptor("gql.reddit.com", "POST", gqlMigrationInterceptor);
 	addInterceptor("gateway.reddit.com", "*", gatewayMigratorInterceptor);
 	addInterceptor("oauth.reddit.com", "*", oauthPipeInterceptor);
+	addInterceptor("www.reddit.com", "*", pipeInterceptor);
 	addInterceptor("/", "POST", relativePathHandler);
 	addInterceptor("reddit-subreddit-uploaded-media.s3-accelerate.amazonaws.com", "POST", s3UploadPipeInterceptor);
 	addInterceptor("reddit-uploaded-media.s3-accelerate.amazonaws.com", "POST", s3UploadPipeInterceptor);
 
 	logger.log("Initialized interceptors.");
+}
+
+const pipeInterceptor: InterceptorHandler = async ({ url, method, headers: requestHeaders }, data = null) => {
+	const response = await window.gmFetch({
+		method: (method as any) || "GET",
+		url,
+		headers: requestHeaders,
+		data: data ?? undefined,
+		anonymous: false
+	} as any);
+
+	return {
+		status: response.status,
+		jsonResponse: response.responseText,
+	}
 }
